2edd96dca30f3b6e5c90a63e15ca322e85c59665
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-11-17 10:00:10 -0500

    List labels for repos

--- reset-labels.py
+++ reset-labels.py
@@ -1,12 +1,23 @@
 import json
 import sys
 import getpass
-import urllib2
 import urllib
+import httplib 
 
-def api_read(path_template, *keys):
-  url = 'https://api.github.com' + path_template % tuple(map(urllib.quote_plus, keys))
-  return json.loads(urllib2.urlopen(url).read())
+import requests
+
+def api_url(path_template, keys):
+  # print path_template % tuple(map(urllib.quote_plus, keys))
+  return 'https://api.github.com' + path_template % tuple(map(urllib.quote_plus, keys))
+
+def api_get(path_template, *keys):
+  url = api_url(path_template, keys)
+  return requests.get(url).json()
+  
+def api_delete(path_template, *keys):
+  url = api_url(path_template, keys)
+  requests.delete(url, auth=(username, password))
+  
 
 repos = sys.argv[2:]
 if not repos:
@@ -16,11 +27,14 @@ team = sys.argv[1]
 username = raw_input('Github username: ')
 password = getpass.getpass()
 
-for repo in sys.argv[2:]:
-  print repo
-  labels = map(lambda label_info: label_info['name'], api_read('/repos/%s/%s/labels', team, repo))
-  labeled_issues = api_read('/repos/%s/%s/issues?filter=all&state=all&labels=%s', team, repo, ','.join(labels))
+for repo in repos:
+  print 'repo: %s' % repo
+  labels = map(lambda label_info: label_info['name'], api_get('/repos/%s/%s/labels', team, repo))
+  print 'labels: %s' % labels
+  labeled_issues = api_get('/repos/%s/%s/issues?filter=all&state=all&labels=%s', team, repo, ','.join(labels))
   if labeled_issues:
     print 'Labels are already in use on issues in %s. Deleting them would be destructive.'
     sys.exit(1)
-    
+  # for label in labels:
+  #   api_delete('https://api.github.com/repos/%s/%s/labels/%s', team, repo, label)
+  
