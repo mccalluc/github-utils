25fa7b9d4cae89b73fda93cf67f0de374d06882a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-11-27 14:48:19 -0500

    Good enough: Create PR by hand in the UI

--- bulk-add-file.py
+++ bulk-add-file.py
@@ -6,40 +6,21 @@ import subprocess
 import os
 
 
-def api_url(path_template, keys):
-    # print path_template % tuple(map(urllib.quote_plus, keys))
-    return 'https://api.github.com' + path_template % tuple(map(urllib.quote, keys))
-
-
-def api_get(path_template, *keys):
-    url = api_url(path_template, keys)
-    return requests.get(url).json()
-
-
-def api_delete(path_template, *keys):
-    url = api_url(path_template, keys)
-    requests.delete(url, auth=(username, password))
-
-def api_create(path_template, team, repo, label, rgb):
-    url = api_url(path_template, (team, repo))
-    requests.post(url, auth=(username, password), json={'name': label, 'color': rgb})
-
-
 repos = sys.argv[2:]
 if not repos:
     print 'USAGE: %s TEAM REPO1 [ REPO2 ... ]' % sys.argv[0]
     sys.exit(1)
 team = sys.argv[1]
-username = raw_input('Github username: ')
-password = getpass.getpass()
+branch = 'add-issue-template'
+message = 'Add issue template'
 
 for repo in repos:
     print 'repo: %s' % repo
     url = 'git@github.com:%s/%s.git' % (team, repo)
     dest = '/tmp/github-utils/%s/%s' % (team, repo)
-    subprocess.check_call(['git', 'clone', url, dest])
+    subprocess.check_call(['git', 'clone', '--depth', '1', url, dest])
     os.chdir(dest)
-    subprocess.check_call(['git', 'checkout', '-b', 'add-one-file'])
+    subprocess.check_call(['git', 'checkout', '-b', branch])
     f = open(dest + '/ISSUE_TEMPLATE.md', 'w')
     f.write('''\
 * Release number or git hash: 
@@ -62,5 +43,5 @@ for repo in repos:
 ''')
     f.close()
     subprocess.check_call(['git', 'add', '.'])
-    subprocess.check_call(['git', 'commit', '-m', 'add issue template'])
-    subprocess.check_call(['git', 'push', 'origin', 'add-one-file'])
+    subprocess.check_call(['git', 'commit', '-m', message])
+    subprocess.check_call(['git', 'push', 'origin', branch])
