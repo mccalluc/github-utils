676a41330b826510df82db551d090fce9d364b3d
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-11-28 16:22:22 -0500

    Will not delete labels already in use. Change label spec to match API output.

--- reset-labels.py
+++ reset-labels.py
@@ -8,11 +8,9 @@ def api_url(path_template, keys):
     # print path_template % tuple(map(urllib.quote_plus, keys))
     return 'https://api.github.com' + path_template % tuple(map(urllib.quote, keys))
 
-
 def api_get(path_template, *keys):
     url = api_url(path_template, keys)
-    return requests.get(url).json()
-
+    return requests.get(url, auth=(username, password)).json()
 
 def api_delete(path_template, *keys):
     url = api_url(path_template, keys)
@@ -34,27 +32,63 @@ password = getpass.getpass()
 for repo in repos:
     print 'repo: %s' % repo
     labels = map(lambda label_info: label_info['name'], api_get('/repos/%s/%s/labels', team, repo))
-    print 'labels: %s' % labels
-    labeled_issues = api_get('/repos/%s/%s/issues?filter=all&state=all&labels=%s', team, repo, ','.join(labels))
-    if labels and labeled_issues:
-        print 'Labels are already in use on issues in %s: %s. Deleting them would be destructive.' % (
-        repo, labeled_issues)
-        sys.exit(1)
+    print 'old labels: %s' % labels
     for label in labels:
-        api_delete('/repos/%s/%s/labels/%s', team, repo, label)
+        labeled_issues = api_get('/repos/%s/%s/issues?filter=all&state=all&labels=%s', team, repo, label)
+        if labeled_issues:
+            print 'Keeping "%s" in "%s" because it is in use' % (label, repo)
+        else:
+            print 'Deleting label "%s"' % label
+            api_delete('/repos/%s/%s/labels/%s', team, repo, label)
 
-new_labels = {
-    # Type
-    'Bug': 'db2d1a',
-    'Technical Debt': '9c2011',
-    'Question': '76429a',
-    'Enhancement': '09457f',
-    'Feature': '1779dc',
-    # Severity
-    'Critical': 'dc6b24',
-    'Important': 'f28f2e'
-}
+new_labels = [ # (Same structure as https://api.github.com/repos/phovea/phovea.github.io/labels)
+  {
+    "name": "Layer: API",
+    "color": "bbbbbb"
+  },
+  {
+    "name": "Layer: Client",
+    "color": "aaaaaa"
+  },
+  {
+    "name": "Layer: Server",
+    "color": "999999"
+  },
+  {
+    "name": "Layer: UI",
+    "color": "cccccc"
+  },
+  {
+    "name": "Severity: Critical",
+    "color": "fbca04"
+  },
+  {
+    "name": "Severity: Important",
+    "color": "fef2c0"
+  },
+  {
+    "name": "Type: Bug",
+    "color": "db2d1a"
+  },
+  {
+    "name": "Type: Enhancement",
+    "color": "09457f"
+  },
+  {
+    "name": "Type: Feature",
+    "color": "1779dc"
+  },
+  {
+    "name": "Type: Question",
+    "color": "76429a"
+  },
+  {
+    "name": "Type: Technical Debt",
+    "color": "9c2011"
+  }
+]
 
 for repo in repos:
-    for label, rgb in new_labels.iteritems():
-        api_create('/repos/%s/%s/labels', team, repo, label, rgb)
+    for label in new_labels:
+        print 'Creating label "%s"' % label['name']
+        api_create('/repos/%s/%s/labels', team, repo, label['name'], label['color'])
